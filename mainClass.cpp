ac3e04356eaec4f86cd5c59c6b583cc4afafa037
OmerEfron <73428465+OmerEfron@users.noreply.github.com>
2023-05-27 18:52:58 +0300

    Update mainClass.cpp

--- mainClass.cpp
+++ mainClass.cpp
@@ -78,38 +78,35 @@ string mainClass::getAirplaneFlight(string icoas24){
     return res;
 }
   
-
 string mainClass::getFull_schedule(vector<string> icoas){
     string res;
-    ostringstream oss;
     vector<string> lines;
-     for (int i = 1; i < icoas.size(); i++)
+    std::ostringstream oss;
+    for (string icoa:icoas)
     {
-    vector<pair<Flight,bool>> sortSche;
-    string name = argv[i];
-//loop sorted by time using func form utilty
+        vector<pair<Flight,bool>> sortSche;
+        string name = icoa;
+    //loop sorted by time using func form utilty
         try {
-            airport air(name);
-            sortSche = air.sort_full_sche();
+            sortSche = airports[name]->sort_full_sche();
             for (const auto& f : sortSche) {
-               std::ostringstream oss;
-                if(f.second == IS_ARV)
-                     oss << "Flight #" << f.first.callsign << " arriving from " << f.first.dpt << ", at " << air.convertLocalTime(f.first.arv_time) << endl;
-                else
-                     oss << "Flight #" << f.first.callsign << " departing to " << f.first.arv << ", at " << air.convertLocalTime(f.first.dpt_time) <<  endl;
-
+                    if(f.second == IS_ARV)
+                        oss << "Flight #" << f.first.callsign << " arriving from " << f.first.dpt << ", at " << airport::convertLocalTime(f.first.arv_time) << endl;
+                    else
+                        oss << "Flight #" << f.first.callsign << " departing to " << f.first.arv << ", at " << airport::convertLocalTime(f.first.dpt_time) <<  endl;
+                    lines.push_back(oss.str());
             }
         }
         catch (string exepction_icoa)
         {
-            cout << exepction_icoa << " not exist in the data base" << endl;
+            res+=exepction_icoa;
         }
-        cout << "----------------------------------------------------------------------------------------------" << endl;
+    }
 	
-	  for (const std::string& line : lines) {
-        //cout<<line <<endl;
+	  for (const std::string& line : lines) 
+      {
         res += line; 
-    }
-    }
+      }
     return res;
+}
 }
