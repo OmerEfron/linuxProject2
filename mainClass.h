4270452a34586c1d838ffaadb9434ea21f48c6cf
OmerEfron <73428465+OmerEfron@users.noreply.github.com>
2023-05-29 11:00:24 +0300

    Add files via upload

--- mainClass.h
+++ mainClass.h
@@ -6,13 +6,15 @@
 class mainClass{
     private:
     std::map<string,airport*> airports;
+    string path = "./flightDB/";
+    string cmd = "./flightScanner.sh";
     
     public:
     mainClass();
     // ~mainClass(); //free airport memory, zips DB;
     std::string getAllArrivalFlightsDetails(std::vector<std::string> icoas); // 1
-    // std::string getFUllAirportScheduleOrderedByTime(std::vector<std::string> airportName); // 2
-    // std::string getAllFlightsOfAircraft(std::vector<string> aircrafts);// 3
-    // bool updateDB();// 4
+    std::string getFull_schedule(std::vector<std::string> icoas); // 2
+    std::string getAirplaneFlight(string icoa24);// 3
+    std::string updateDB();// 4
     // bool zipDB(); // 5
 };
